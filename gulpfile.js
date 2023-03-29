f4e5ec268319dbe5d25cbb1bb5a612aefb0602a3
dumplant <2437789306@qq.com>
2023-03-29 14:42:58 +0800

    change package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,11 @@ const gulp = require('gulp')
 const sass = require("gulp-sass")(require("sass"));
 const minifyCSS = require('gulp-minify-css')
 gulp.task('sass', async function () {
-    try {
+   
         return gulp
       .src("components/css/**/*.scss")
       .pipe(sass())
       .pipe(minifyCSS())
       .pipe(gulp.dest("dist/css"));
-    } catch (e) {
-        console.log(e)
-    }
-    
+   
 })
