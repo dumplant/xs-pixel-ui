dbafe8395cea9fc5259d42974c0a65066ecb64f4
dumplant <2437789306@qq.com>
2023-03-31 02:19:53 +0800

    fear: finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,12 @@
 module.exports = {
   title: "xs-pixel-ui",
+  base: "/xs-pixel-ui/",
   themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Github", link: "https://github.com/dumplant/xs-pixel-ui" },
+      { text: "NPM", link: "https://www.npmjs.com/package/xs-pixel-ui" },
+    ],
     sidebar: ["/", "/componentsDocs/Button", "/componentsDocs/Card"],
   },
 };
